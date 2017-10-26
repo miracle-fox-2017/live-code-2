7aad352806bf1ba28e922e00f7fccf6f43c8fab9
ipunganwar <ipung.anwar1@gmail.com>
2017-10-26 10:17:31 +0700

    live-code 2

--- logic/domino.js
+++ logic/domino.js
@@ -1,1 +1,27 @@
 //your code here
+'use strict'
+function dominoCard(card){
+	let kartu = card[1].sort(),
+		terbesar = kartu[1],
+		hasil = []
+
+	for(let i = 0; i < card[0].length; i++){
+		
+		for(let j = 0; j < card[0][i].length; j++){
+
+			if(card[0][i][j] == terbesar && j > 0){
+				hasil.push(card[0][i])
+			}
+			else if(card[0][i][j] == card[0][i][j+1] && card[0][i][j] > terbesar){
+				hasil.push(card[0][i])
+			}
+		}
+	}
+
+	console.log(hasil)
+
+}
+
+dominoCard([[[3,3],[6,5],[3,4],[2,1]],[3,5]]) //keluarkan kartu 6,5
+dominoCard([[[3,3],[6,5],[3,4],[2,1]],[1,3]]) //keluarkan kartu 6,5
+dominoCard([[[2,4],[6,6],[3,6]],[1,5]]) //tutup 1 kartu
--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,1 +1,29 @@
 //your code here
+'use strict'
+
+function getCombinations(str, arr = []){
+
+	if(str.length == 1){
+		return arr
+	}
+	else{
+		// console.log(str)
+
+		for(let i = 0; i < str.length; i++){
+			// console.log(str[i])
+			let huruf = ''
+			for(let j = 0; j <= i; j++){
+				huruf += str[j]
+			}
+			
+			arr.push(huruf)
+			
+		}
+		
+		return getCombinations(str.slice(1), arr)
+	}
+
+}
+
+
+console.log(getCombinations('cros'))
