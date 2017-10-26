a8612a348b2fe047b877b98c5fa7b3ed8820916b
aditReza <lete.note3@gmail.com>
2017-10-26 10:28:32 +0700

    -1

--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,6 +1,13 @@
-//your code here
-function getCombinations(values){
+function getCombinations(values) {
+  let container = []
+  for (let i = 0; i < values.length; i++) {
+      for (let j = i + 1; j < values.length + 1; j++) {
+          container.push(values.slice(i, j));
+      }
+  }
+    return container;
 
 }
-
-getCombinations('cros')
+// console.log(getCombinations('cros'));
+console.log(getCombinations('artic'));
+// console.log(getCombinations('darwin'));
