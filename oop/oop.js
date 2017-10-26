275970b3cfa36c6f4ef1c1ad8dd619e7dd3eaae1
rezaalfarabi <anangreza18@gmail.com>
2017-10-26 11:41:29 +0700

    live code

---
+++ oop/oop.js
@@ -0,0 +1,57 @@
+class Hero {
+    constructor (param) {
+        this._health = health;
+        this._mana = mana;
+        this._defense = defense;
+    }
+
+} 
+
+class Assasin extends hero {
+    super(param)
+    constructor(Name, health, Mana, Defense) {
+        this.Name = "Assasin" ;
+        this.health = 3213;
+        this.Mana = 126;
+        this.Defense = 431; 
+    }
+}
+
+class Knight extends Hero {
+    super(param)
+    constructor(Name, health, Mana, Defense) {
+        this.Name = "Knight" ;
+        this.health = 3213;
+        this.Mana = 126;
+        this.Defense = 431; 
+    }
+}
+
+class Mage extends Hero {
+    super(param)
+    constructor(Name, health, Mana, Defense) {
+        this.Name = "Mage" ;
+        this.health = 1130;
+        this.Mana = 603;
+        this.Defense = 231; 
+        
+    }   
+
+   
+}
+
+
+class SummonHero {
+    constructor (param) {
+
+    } 
+    getAcak () {
+        this.height ++
+        }
+
+    getPanggil() {
+
+    }
+}
+
+
