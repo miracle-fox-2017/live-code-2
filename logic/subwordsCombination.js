09c543f007f5b0575c7797559974c05985a5380d
Faris Widyantho <radenfaris@gmail.com>
2017-10-26 10:16:02 +0700

    commit 2

--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -2,18 +2,32 @@ function getCombinations(value){
   //code
   let result = [];
   let pecah = value.split('')
+  let bank = []
   // console.log(pecah);
   for(let i = 0; i<pecah.length; i++){
     let tampung = [];
-    let count = 0;
     for(let j = i; j<pecah.length; j++){
-      while(count<i){
-        tampung.push(pecah[j])
-        count++;
-      }
-      console.log(tampung);
+      tampung.push(pecah[j]);
     }
+    bank.push(tampung.join(''));
   }
+  // console.log(bank);
+  
+  
+  for(let i = 0; i<bank.length; i++){
+    let count = 0;
+    let tampung = []
+    for(let j = i; j<bank[i].length; j++){
+      while(count<=j){
+        tampung.push(bank[i][count])
+        count += 1;
+      }
+      result.push(tampung.join(''));
+    }
+
+
+  }
+
   
   
   
@@ -21,3 +35,5 @@ function getCombinations(value){
 }
 
 console.log(getCombinations('cros'));
+console.log(getCombinations('artic'));
+console.log(getCombinations('darwin'));
