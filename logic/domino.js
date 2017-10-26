4d3d73eb2cdbf8fea32b71b7d2ccb312ce056c66
AhmadNizar <ahmadnizar.owl@gmail.com>
2017-10-26 10:20:02 +0700

    fix

--- logic/domino.js
+++ logic/domino.js
@@ -5,7 +5,7 @@ function dominoCard(nesArr, arr){
 	let check1 = arr[0]
 	let check2 = arr[1]
 	let tempResult = []
-	let indexResult = []
+	let indexResult = [0]
 	let max_result = 0 
 
 	nesArr.forEach(input =>{
@@ -22,15 +22,18 @@ function dominoCard(nesArr, arr){
 		indexResult[index] = sum[0]+sum[1]
 	})
 
-	var max = indexResult.reduce(function(a, b) {
-    	return Math.max(a, b);
-	});
+	// console.log(indexResult);
+	if(indexResult[0] != 0){
+		var max = indexResult.reduce(function(a, b) {
+    		return Math.max(a, b);
+		});	
+	}
 
-	if(max!=0){
+	if(max!=undefined){
 		console.log('keluarkan kartu ['+tempResult[indexResult.indexOf(max)]+']');
 	}else{
 		console.log('tutup 1 kartu');
 	}
 }
 
-dominoCard([[3,3],[6,5],[3,4],[2,1]], [3,5])
+dominoCard([[3,3],[6,5],[3,4],[2,1]], [0,0])
