00a9bbae3e87976349c1046708dd53ce3b12a5e1
vbagustinus <vbagustinus@gmail.com>
2017-10-26 10:30:07 +0700

    Bkn buat nilai

--- logic/domino.js
+++ logic/domino.js
@@ -1,5 +1,7 @@
 function dominoCard(card, cardTable) {
   let data = []
+  let result = []
+  let tampung= []
   for (var i = 0; i < card.length; i++) {
     if(card[i][0]===cardTable[0] || card[i][1]===cardTable[1] || card[i][0]===cardTable[1] ||card[i][1]===cardTable[0]){
       data.push(card[i])
@@ -9,13 +11,12 @@ function dominoCard(card, cardTable) {
     return `tutup 1 kartu`
   } else {
     for (var i = 0; i < data.length; i++) {
-      data[i]
+      tampung.push(data[i][0]+data[i][1])
     }
   }
+  let index = tampung.indexOf(Math.max(...tampung))
+  return data[index]
 }
-
-
-
 console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [3,5])) //”keluarkan kartu [6,5]”
 console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [1,3])) //"keluarkan kartu [3,4]"
 console.log(dominoCard([[2,4],[6,6],[3,6]], [1,5])) //”tutup 1 kartu”
