99e69933e16f5d73d621a20ddfc3c392560cc895
Nandira Paturohman <nandirasp@gmail.com>
2017-10-26 10:16:10 +0700

    livecode 2

--- logic/domino.js
+++ logic/domino.js
@@ -1,1 +1,13 @@
-//your code here
+dominoCard(arr, domino) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      if(arr[i][j] == domino[0] || arr[i][j] == domino[1]){
+        
+      }
+    }
+  }
+}
+
+dominoCard([[3,3],[6,5],[3,4],[2,1]], [3,5]) //”keluarkan kartu [6,5]”
+dominoCard([[3,3],[6,5],[3,4],[2,1]], [1,3]) //"keluarkan kartu [3,4]"
+dominoCard([[2,4],[6,6],[3,6]],[1,5]) //”tutup 1 kartu”
