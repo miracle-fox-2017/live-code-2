d915d9d729694e42221db8a963d6367d519e08f7
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-26 10:58:03 +0700

    done

--- oop/summonhero.js
+++ oop/summonhero.js
@@ -23,14 +23,3 @@ class SummonHero {
 }
 
 module.exports = SummonHero;
-
-
-// driver code
-// console.log(SummonHero.panggil())
-// console.log(SummonHero.acak())
-//output yang diharapkan (output akan random jadi tidak perlu sesuai dengan contoh ini)
-//Mage {name: Assassin, health: 1130, mana: 603, defense: 231}
-
-// SummonHero.panggil().skill()
-//output yang diharapkan (output akan random jadi tidak perlu sesuai dengan contoh ini)
-//‘Assassin mengeluarkan skill: Ciat..! Serangan tanpa bayangan..’
