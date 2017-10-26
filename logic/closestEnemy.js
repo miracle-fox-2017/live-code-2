3e0a0e6981c9917434c04c6442354ea0dcb0a8a5
Ian Max <ianmax1983@gmail.com>
2017-10-26 10:13:55 +0700

    live

--- logic/closestEnemy.js
+++ logic/closestEnemy.js
@@ -1,1 +1,28 @@
 //your code here
+
+let count = 0;
+
+function closest(arr) {
+  let answer;
+  for (let x = 0; x < arr.length; x++){
+    if (arr.indexOf(2) == -1) {
+      answer = 'There is no enemy'
+    } else if (arr.indexOf(2) != -1) {
+      checkKanan(arr);
+    }
+    return answer;
+  }
+}
+
+function checkKanan(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == 0) {
+      count++;
+    }
+  }
+  return count+1;
+}
+
+
+console.log(closest([1, 0, 0, 0, 2, 2, 2]));
+console.log(closest([0, 0, 0, 1, 0]));
--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,1 +1,15 @@
 //your code here
+function getCombinations(value) {
+let answer = [];
+let kombinasi;
+  for (var i = 0; i < value.length; i++) {
+      for (var j = 0; j <= i; j++) {
+        answer.push(value.slice(j, value.length - i + j))
+      }
+    }
+  console.log(answer.sort());
+}
+
+
+getCombinations('cros');
+getCombinations('darwin');
