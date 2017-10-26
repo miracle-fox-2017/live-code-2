2e50251ff5ab47956dd67c194f55b2bc9be200c2
Angga Priambada <angga.idabagus@gmail.com>
2017-10-26 10:23:35 +0700

    commited

--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,1 +1,18 @@
 //your code here
+function getCombinations(value) {
+    //write your code here
+    let arrHasil = []
+    
+    for(var i = 0; i < value.length;i++){
+        let tempWord = ""
+        tempWord += value[i]
+        arrHasil.push(tempWord)
+        for(var j = i+1; j < value.length;j++){
+            tempWord += value[j]
+            arrHasil.push(tempWord)
+        }
+    }
+    console.log(arrHasil)
+} 
+
+getCombinations('artic')
