e11ba8cff3d4dbd7a33b2c38b26cdfadbec64d80
trijadoel <triamri@gmail.com>
2017-10-26 11:01:58 +0700

    opp

---
+++ oop/assasin.js
@@ -0,0 +1,10 @@
+class Assassin extends Hero {
+    constructor(name, health, mana, bertahan) {
+        super(name, health, mana, bertahan);
+    }
+
+    skill() {
+        console.log("Ciat..! Serangan tanpa bayangan..");
+    }
+
+}
---
+++ oop/knight.js
@@ -0,0 +1,10 @@
+class Knight extends Hero {
+    constructor(name, health, mana, bertahan) {
+        super(name, health, mana, bertahan);
+    }
+
+    skill() {
+        console.log("Lemparan Perisai Suci");
+    }
+
+}
