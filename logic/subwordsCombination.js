6f2786b1fee9a283194b4a06b526d9edf4353e3b
alang <alangmahendra@gail.com>
2017-10-26 10:16:37 +0700

    live-code2

--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,1 +1,9 @@
-//your code here
+function words(str) {
+  let tam = []
+  for (var i = 0; i < str.length; i++) {
+    let arr = str.split('')
+    tam.push(arr[i],arr[i]+arr[i+1])
+  }
+  return tam
+}
+console.log(words('cross'));
