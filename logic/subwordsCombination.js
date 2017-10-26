4905d5e73e928369fc96c081eb618033828409b0
Ahmad <matt.syahab@gmail.com>
2017-10-26 10:02:03 +0700

    salah judul

--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,31 +1,21 @@
 //your code here
-function dominoCard(arr,arr2){
-	let totalCard = []
-	let tempResult = 0
-	let result = null
-	for (let i = 0; i<arr.length ; i++){
-		let total = 0;
-		for (let j = 0 ; j<arr[i].length ; j++){
-			total = total + arr[i][j]
+function getCombinations(value) {
+	//write your code here
+	let arr = [];
+	let valueA = value.split("")
+	let result = []
+	let temp = ''
+	for (let i = 0 ; i<valueA.length ; i++){
+		for (let j = i ; j<valueA.length ; j++){
+		temp = temp+valueA[j]
+		result.push(temp)
 		}
-		totalCard.push(total)
+		temp = ''
 	}
-	for (let i = 0 ; i<arr2.length ; i++){
-		for (let j = 0 ; j<arr.length ; j++){
-			if (arr[j].indexOf(arr2[i]) !== -1){
-				if (totalCard[j] > tempResult){
-					tempResult = totalCard[j]
-					result = j
-				}
-			}
-		}
-	}
-	if (result === null){
-		return "tutup 1 kartu"
-	}
-	return "keluarkan kartu ["+arr[result]+"]"
-}
+	return result;
+} 
 
-console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [3,5])) //”keluarkan kartu [6,5]”
-console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [1,3])) //"keluarkan kartu [3,4]"
-console.log(dominoCard([[2,4],[6,6],[3,6]],[1,5])) //”tutup 1 kartu”
+
+console.log(getCombinations('cros'))
+// console.log(getCombinations('artic'))
+// console.log(getCombinations('darwin'))
