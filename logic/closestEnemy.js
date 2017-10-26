ba83d3a15b7e1fe673aa9b1a344ce2f5fa9c7770
agungpangestu <agung_pangestu83@ymail.com>
2017-10-26 10:16:13 +0700

    Live Code2

--- logic/closestEnemy.js
+++ logic/closestEnemy.js
@@ -1,1 +1,19 @@
 //your code here
+
+function closesEnemy(arr) {
+  let indexPrajurit = arr.indexOf(1)
+  let indexMusuh =arr.indexOf(2)
+  if (indexPrajurit > indexMusuh){
+    let tampung = indexPrajurit - indexMusuh;
+    console.log(arr);
+    console.log(`Your closesEnemy ${tampung} space on the Left position`);
+  }
+  else {
+    let tampungMusuh = indexMusuh - indexPrajurit;
+    console.log(arr);
+    console.log(`Your closesEnemy ${tampungMusuh} space on the right position`);
+
+  }
+}
+
+closesEnemy([1,0,0,0,0,2,2,2])
--- logic/domino.js
+++ logic/domino.js
@@ -1,1 +1,32 @@
-//your code here
+function dominoCard(arr,value) {
+  var angka = 0
+  var angkaditangan = 0
+  var counter =0
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[0] == value[0] || arr[0] == value[1] || arr[1] == value[0] || arr[1] == value[1]){
+
+      let tambahkartu = arr[0]+arr[1]
+      counter = counter +1
+      if(counter > 1){
+
+        if (tambahkartu > angka){
+          // console.log(angka);
+          return console.log("Keluarkan Kartu" + arr[i]);
+        }
+
+      }
+      else {
+        console.log(angka);
+        angka = tambahkartu
+      }
+
+      // else {
+      //   return console.log("tutup 1 Kartu");
+      }
+    }
+    return console.log("tutup 1 Kartu")
+  }
+
+dominoCard([[3,3],[6,5],[3,4],[2,1]],[3,5]) //”keluarkan kartu [6,5]”
+dominoCard([[3,3],[6,5],[3,4],[2,1]],[1,3]) //"keluarkan kartu [3,4]"
+dominoCard([[2,4],[6,6],[3,6]],[1,5]) //”tutup 1 kartu”
