964e3ada8c56084ae04874bea11035d346cb3863
Ahmad <matt.syahab@gmail.com>
2017-10-26 09:55:00 +0700

    All Done

--- logic/closestEnemy.js
+++ logic/closestEnemy.js
@@ -1,1 +1,42 @@
 //your code here
+function closeEnemy(arr){
+	let tempPrajurit ;
+	let musuh = []
+	let tempJarak = 9999;
+	let position ;
+	for (let i = 0 ; i<arr.length ; i++){
+		if (arr[i] === 1){
+			tempPrajurit = i;
+		}
+		if (arr[i] === 2){
+			musuh.push(i)
+		}
+	}
+	if (musuh.length == 0){
+		return "there is no enemy"
+	}
+	for (let j = 0 ; j<musuh.length ; j++){
+		let temp = Math.abs(tempPrajurit-musuh[j])
+		if (temp < tempJarak){
+			tempJarak = temp
+		}
+	}
+
+	if (tempJarak < tempPrajurit ){
+		position = "left";
+	}
+
+	if (tempJarak > tempPrajurit){
+		position = "right";
+	}
+
+	if (tempJarak === tempPrajurit){
+		position = "left and right";
+	}
+	return  "your closest enemy "+tempJarak+" space on the "+position+" from your position"
+}
+
+console.log(closeEnemy([1,0,0,0,2,2,2]))
+console.log(closeEnemy([0,0,0,0,2,1,0]))
+console.log(closeEnemy([2,0,0,1,0,0,2]))
+console.log(closeEnemy([0,0,0,0,0,1,0]))
--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,1 +1,31 @@
 //your code here
+function dominoCard(arr,arr2){
+	let totalCard = []
+	let tempResult = 0
+	let result = null
+	for (let i = 0; i<arr.length ; i++){
+		let total = 0;
+		for (let j = 0 ; j<arr[i].length ; j++){
+			total = total + arr[i][j]
+		}
+		totalCard.push(total)
+	}
+	for (let i = 0 ; i<arr2.length ; i++){
+		for (let j = 0 ; j<arr.length ; j++){
+			if (arr[j].indexOf(arr2[i]) !== -1){
+				if (totalCard[j] > tempResult){
+					tempResult = totalCard[j]
+					result = j
+				}
+			}
+		}
+	}
+	if (result === null){
+		return "tutup 1 kartu"
+	}
+	return "keluarkan kartu ["+arr[result]+"]"
+}
+
+console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [3,5])) //”keluarkan kartu [6,5]”
+console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [1,3])) //"keluarkan kartu [3,4]"
+console.log(dominoCard([[2,4],[6,6],[3,6]],[1,5])) //”tutup 1 kartu”
