f5fb74d7804bc4e87628d0fb7574c2ad06796eb9
albertagung <albiardtya@gmail.com>
2017-10-30 10:14:43 +0700

    albertagung

--- logic/subwordsCombination.js
+++ logic/subwordsCombination.js
@@ -1,1 +1,13 @@
 //your code here
+function getCombinations(value){
+  let arr = [];
+  for(let i = 0; i < value.length; i++){
+    // let valueI = value[i]
+    for(let y = 0; y < value.length-1; y++){
+      console.log(value[y]);
+      arr.push(value[y]+value[y+1])
+    }
+  }
+  console.log(arr);
+}
+getCombinations('cros')
